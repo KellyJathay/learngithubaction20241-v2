d7b9fbc8ad16f06d9e9594cc59932e58268d4226
Kelly Jathay <kelly.jathay@gmail.com>
2024-08-03 20:47:05 +0100

    Config lighthouse url V2

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-            url: ['https://localhost:3000/'],
+            url: ['https://www.saucedemo.com/'],
             numberOfRuns: 3,
             settings: {
                 onlyCategories: ['accessibility'],
